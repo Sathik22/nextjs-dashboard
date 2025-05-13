b1097fdef37d6e6bbcaf63f3f132dd172c898c82
Sathik22 <mohamedjahabarsathik2001@gmail.com>
2025-05-13 15:25:01 +0530

    first commit

---
+++ app/ui/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+export default function Page() {
+    return(
+    <>
+        <h1>Dashboard Page</h1>
+{
+        /*<Link href="/dashboard/customer"> <button className=" md:w-[250px] bg-blue-500 p-2 rounded-md"> 
+        Customer Page </button></Link>*/
+        }
+    </>)
+  }
---
+++ app/ui/invoices/page.tsx
@@ -0,0 +1,11 @@
+import InvoiceStatus from "./status"
+
+export default function InvoicesPage() {
+
+    return(
+        <>
+        <InvoiceStatus status="pending" />
+        
+        </>
+    )
+}
